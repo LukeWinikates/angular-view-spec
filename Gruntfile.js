67f3dd35b1533d7b9f5be0708b3cf49c75edc39a
Luke Winikates <luke.winikates@gmail.com>
2014-10-19 14:20:34 -0700

    ensure that jQuery is loaded before angular so that triggering events with jQuery works
     -- see: http://stackoverflow.com/a/17962618/386396

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,9 @@ module.exports = function(grunt){
         options: {
           specs: 'spec/**/*Spec.js',
           helpers: [
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/jquery/dist/jquery.js',
             'spec/support/*.js'
           ]
         }
